37abd2b9f13f80ce636301223d03d4f47ff8fe4a
TilawatArnaud <tilawatarnaud@gmail.com>
2025-05-05 10:53:28 +0200

    adding errors handling

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,11 +2,25 @@
 import sys
 
 def factorial(n):
+    """Return the factorial of a given positive integer n."""
     result = 1
     while n > 1:
         result *= n
         n -= 1
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./factorial.py <number>")
+        sys.exit(1)
+
+    try:
+        num = int(sys.argv[1])
+        if num < 0:
+            print("Factorial is not defined for negative numbers")
+            sys.exit(1)
+    except ValueError:
+        print("Please provide a valid integer")
+        sys.exit(1)
+
+    print(factorial(num))
